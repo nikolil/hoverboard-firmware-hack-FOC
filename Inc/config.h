4242bfff26979ee1ac91074badba1ed783c504c8
Nikolay Illarionov <89645062746@yandex.ru>
2026-06-30 12:50:33 +0300

    ==

--- Inc/config.h
+++ Inc/config.h
@@ -179,6 +179,7 @@
 // ############################## DEFAULT SETTINGS ############################
 // Default settings will be applied at the end of this config file if not set before
 #define INACTIVITY_TIMEOUT        8       // Minutes of not driving until poweroff. it is not very precise.
+// TODO Сигнал при движении назад.
 #define BEEPS_BACKWARD            0 //1       // 0 or 1
 #define ADC_MARGIN                100     // ADC input margin applied on the raw ADC min and max to make sure the MIN and MAX values are reached even in the presence of noise
 #define ADC_PROTECT_TIMEOUT       100     // ADC Protection: number of wrong / missing input commands before safety state is taken
